d8e26fb1a249d53870ac4878a8e4179315920d1e
Your Name <you@example.com>
2023-02-18 16:15:52 -0800

    Print and ommit

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program thst prints the slphabet in lowercase without q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+	if (alp == 'e' || alp == 'q')
+		continue;
+		putchar(alp);
+
+		putchar('\n');
+	return (0);
+	}
+}
