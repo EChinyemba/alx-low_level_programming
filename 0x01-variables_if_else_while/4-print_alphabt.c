4c9c03ce64687fd2802ff174dea356a0c8a1c7a8
Your Name <you@example.com>
2023-02-18 16:44:45 -0800

    maybe this one

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,16 +3,16 @@
 #include <stdlib.h>
 
 /**
- * main - Program thst prints the slphabet in lowercase without q and e
+ * main - Program that prints the alphabet in lowercase without q and e
  * Return: 0
  */
 int main(void)
 {
 	char alp;
 
-	for (alp = 'a'; alp <= 'z'; alp++)
+	for(alp = 'a'; alp <= 'z'; alp++)
 	{
-	if (alp == 'e' || alp == 'q')
+	if(alp == 'e' || alp == 'q')
 		continue;
 		putchar(alp);
 
