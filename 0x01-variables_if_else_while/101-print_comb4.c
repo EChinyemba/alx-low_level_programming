8940442e0309bed11a00db5cf0b35d674d692654
Your Name <you@example.com>
2023-02-20 12:53:07 -0800

    Pushing my luck

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,16 +15,19 @@ for(b = a + 1; b < 9; b++)
 
 for(c = b + 1; c < 10; c++)
 {
-if(a != b && a != c && b != c)
+
+	if(a != b && a != c && b != c)
 
 	putchar(a + '0');
 	putchar(b + '0');
 	putchar(c + '0');
-if(a + b + c < 24)
-{
+
+	if(a + b + c < 24)
+	{
+
 	putchar(',');
 	putchar(' ');
-}
+	}
 
 }
 	putchar('\n');
