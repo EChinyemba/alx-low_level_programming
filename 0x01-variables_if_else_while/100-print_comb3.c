00a3b5f5557b1177944c8d1b16449232f3c743bb
Your Name <you@example.com>
2023-02-20 11:12:55 -0800

    Another one

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,18 @@ int main(void)
 	int a;
 	int b;
 
-for (a = 0; a <= 8; a++)
+for(a = 0; a <= 8; a++)
 
-for (b = a + 1; b <= 9; b++)
+for(b = a + 1; b <= 9; b++)
 
 	if (a != b)
 	{
 		putchar(a + '0');
 		putchar(b + '0');
+
 		if (a + b == 89)
 			break;
+
 		if (a + b < 89)
 		{
 			putchar(',');
