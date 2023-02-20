7dacfc17c2b1480ffae4dd7c4f6c832fea27806c
Your Name <you@example.com>
2023-02-20 11:47:51 -0800

    Once more

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,21 +14,22 @@ for(a = 0; a <= 8; a++)
 
 for(b = a + 1; b <= 9; b++)
 
-	if (a != b)
-	{
-		putchar(a + '0');
-		putchar(b + '0');
+if (a != b)
+{
+	putchar(a + '0');
+	putchar(b + '0');
 
-		if (a + b == 89)
-			break;
+if (a + b == 89)
+	break;
 
-		if (a + b < 89)
-		{
-			putchar(',');
+if (a + b < 89)
+{
+	putchar(',');
 		
-			putchar(' ');
-		}
-	}
+	putchar(' ');
+}
+
+}
 	
 	putchar('\n');
 
