1e731df021506030e4e3299dca84df83bc76cce4
Your Name <you@example.com>
2023-02-22 12:10:30 -0800

    Main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,18 @@
-void print_to_98(int n)
-int add(int, int)
-void times_table(void)
-void jack_bauer(void)
-int print_last_digit(int)
-int _abs(int)
-int print_sign(int n)
-int _isalpha(int c)
-int _islower(int c)
-void print_alphabet_x10(void)
-void print_alphabet(void)
+#include <stdio.h>
+#include <unistd.h>
 
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
