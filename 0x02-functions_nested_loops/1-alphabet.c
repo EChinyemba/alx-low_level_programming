545a5c172e3cb2c5c34347609fa77b197e8ad281
Your Name <you@example.com>
2023-02-23 04:11:46 -0800

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry
+ * Return: always 0
+ */
+int main()
+{
+char alp;
+
+for(alp = 'a'; alp <= 'z'; alp++)
+
+	_putchar(alp);
+	_putchar('\n');
+
+return (0);
+}
