71aaebb16796a2c8c9750724b52c77369d8776af
Your Name <you@example.com>
2023-02-23 14:37:06 -0800

    10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,17 +4,19 @@
  * main - Prints alphabet 10 times in lowercase
  * Return: nothing
  */
-void print_alphabet_×10(void)
+void print_alphabet_x10(void)
 {
 	char alp;
 	int i;
 
-	for (i = 1; i <= 10; i++)
+	for(i = 1; i <= 10; i++)
 	{
-	for (alp = 'a'; alp <= 'z'; alp++)
+
+		for(alp = 'a'; alp <= 'z'; alp++)
 		_putchar(alp);
 
 		_putchar('\n');
 	}
+
 
 }
