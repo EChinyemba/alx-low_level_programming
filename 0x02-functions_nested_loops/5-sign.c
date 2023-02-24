419d9ece711771ddfef0912a0aba990dbb842dc7
Your Name <you@example.com>
2023-02-23 17:24:44 -0800

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - shows sign of a number
+ * Return: nothing
+ */
+int print_sign(int n)
+{
+int x = 0;
+
+if(n > 0)
+_putchar('+');
+
+	return 1;
+
+ if(n == 0)
+ {
+ _putchar(x + '0');
+
+	return 0;
+ }
+else
+_putchar('-');
+
+	return -1;
+}	
+	
